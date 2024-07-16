d9ecabeed20f3cde2ce8a20a348b9da8c2d53f18
Irvin Fernandez <ifernandez.estevezjor@gmail.com>
2024-07-16 11:30:57 -0600

    Sección 15: CRUD - Cuadro de Diálogo

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, switchMap, tap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
     id: new FormControl<string>(''),
@@ -23,5 +29,94 @@ export class NewPageComponent {
     {id: 'DC Comics', desc: 'DC - Comics'},
     {id: 'Marvel Comics', desc: 'Marvel - Comics'},
   ];
+
+
+  //Método para guardar información del formulario
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar,
+    private dialog: MatDialog
+  ){}
+  
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+  
+  ngOnInit(): void {
+    if(!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroById(id)),
+    ).subscribe(hero => {
+      if(!hero){
+        return this.router.navigateByUrl('/');
+      }
+      this.heroForm.reset(hero);
+      return;
+    });
+  }
+
+  onSubmit(): void{
+    
+    if(this.heroForm.invalid) return;
+
+    if(this.currentHero.id){
+      this.heroesService.updateHero(this.currentHero)
+      .subscribe(hero => {
+        this.showSnackbar(`${hero.superhero} updated!`);
+      });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero)
+    .subscribe( hero => {
+      this.router.navigate(['/heroes/edit', hero.id]);
+      this.showSnackbar(`${hero.superhero} created!`);
+    });
+    
+  }
+
+
+  //Mostrar cuadro de diálogo
+  onDeleteHero(){
+    if(!this.currentHero.id) throw Error('Hero is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value,
+      height: '200px',
+      width: '500px',
+    });
+
+    dialogRef.afterClosed()
+    .pipe(
+      filter( (result: boolean) => result),
+      switchMap( () => this.heroesService.deleteHeroById(this.currentHero.id)),
+      filter( (wasDeleted: boolean) => wasDeleted)
+    )
+    .subscribe(result => {
+      this.router.navigate(['/heroes']);
+    });
+    
+    /* dialogRef.afterClosed().subscribe(result => {
+      if(!result) return;
+      this.heroesService.deleteHeroById(this.currentHero.id);
+      this.router.navigate(['/heroes']);
+    }); */
+
+  }
+
+
+  //Mostrar snackbar
+  showSnackbar(message: string): void{
+    this.snackbar.open(message, 'done', {
+      duration: 2500,
+    })
+  }
+
 
 }
